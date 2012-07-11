36083c2ca5c4607e9d46ba9e23ac15b352c6e976
Dominic Tarr <dominic.tarr@gmail.com>
2012-07-11 22:12:55 +1200

    redo every thing as a bash script

---
+++ gen.sh
@@ -0,0 +1,45 @@
+#! /usr/bin/env bash
+
+. ~/.gen/config
+
+name=$2
+arg=$3
+if [ "$name" = -f ]; then
+  name=''
+fi
+if [ "x$name" = x ]; then
+  dir=`readlink . -e`
+  name=`basename $dir`
+  arg=$2
+fi
+
+create () {
+  curl -X POST -u "$username:$auth" "https://api.github.com/user/repos" -d "{\"name\":\"$name\"}"
+}
+
+ls () {
+  gh GET /user/repos
+}
+
+delete () {
+  if [ "$arg" != "-f" ]; then
+    echo WARNING YOU ABOUT TO DELETE $username/$name A YOUR SURE: Y/n?
+    read rusure
+    case $rusure in 
+      Y) echo OKAY, $rusure DELETING: $username/$name ;;
+      *) exit 1 ;;
+    esac 
+  fi
+  #gh DELETE "/repos/$username/$name" 
+  curl -X DELETE -u "$username:$auth" "https://api.github.com/repos/$username/$name"
+}
+
+add () {
+  if [ "$arg" = -f ]; then
+    git remote rm origin >2 
+  fi
+  git remote add origin "git@github.com:$username/$name.git" >2
+}
+
+"$@"
+
