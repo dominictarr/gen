90decb20e8eb55c6d5e5c5939b303fd781c6a1cc
Dominic Tarr <dominic.tarr@gmail.com>
2012-08-05 17:35:28 +0200

    use rc for config

--- cli.js
+++ cli.js
@@ -1,7 +1,13 @@
 #! /usr/bin/env node
 
 var replace = require('./replace')
-var opts = require('optimist').argv
+var opts = require('rc')('gen', {
+  year: new Date().getFullYear()
+})
+
+if(!opts.name)
+  new Error("nameless module does not compute")
+
 process.stdin.resume()
 process.stdin
   .pipe(replace(opts))
